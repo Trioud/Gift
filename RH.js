c99b83f7a0091648de9a2f745152a31c21da02fd
lobbix <adrien.lopez@epitech.eu>
2025-12-22 16:59:11 +0100

    feature: adding RH

--- RH.js
+++ RH.js
@@ -1,7 +1,51 @@
 import { game, updateUI } from "./gamelogic.js";
+import { upgrades as marketingUpgrades } from "./marketing.js";
 
-window.augmenterTempsDeTravail = () => {
-  if (game.cdf >= 1) {
+const upgrades = {
+  elfEfficiency: { price: 1, limit: 1 },
+  marketingEfficiency: { price: 1, limit: 1 },
+  marketingUpgrade: { price: 1, limit: 3 },
+  elfSchedule: { price: 1, limit: 5 },
+  nightClub: { price: 1, limit: 1 },
+  sabotageCoin: { price: 5, limit: -1 },
+};
+
+function buyUpgrade(key, effectFn) {
+  const upgrade = upgrades[key];
+
+  if (upgrade.limit === 0) return;
+  if (game.cdf < upgrade.price) return;
+
+  game.cdf -= upgrade.price;
+
+  if (upgrade.limit > 0) {
+    upgrade.limit -= 1;
+  }
+
+  effectFn();
+  updateUI();
+}
+
+window.elfEfficiency = () => {
+  buyUpgrade("elfEfficiency", () => {
+    game.elfEfficiency += 1;
+  });
+};
+
+window.marketingEfficiency = () => {
+  buyUpgrade("marketingEfficiency", () => {
+    game.eurosPerChild += 2;
+  });
+};
+
+window.marketingUpgrade = () => {
+  buyUpgrade("marketingUpgrade", () => {
+    marketingUpgrades.upgradeTier += 1;
+  });
+};
+
+window.elfSchedule = () => {
+  buyUpgrade("elfSchedule", () => {
     if (game.workStartHour > 0) game.workStartHour--;
 
     if (game.workEndHour < 24) game.workEndHour++;
@@ -9,8 +53,7 @@ window.augmenterTempsDeTravail = () => {
     if (game.workStartHour === 0 && game.workEndHour === 24) {
       game.fullTime = true;
     }
-    game.cdf -= 1;
-  }
+  });
 
   updateUI();
 };
